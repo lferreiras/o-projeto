f36172559ee8038bb4af2ad61879c40b9b83bf9c
lferreiras <135626437+lferreiras@users.noreply.github.com>
2023-07-06 00:04:19 -0300

    Add files via upload

---
+++ exe.2.2.py
@@ -0,0 +1,26 @@
+# %%
+# %%
+
+notas=[0,0,0,0]
+
+i=0
+while i < 4:
+    notas[i]=float(input(f"digite a {i}° nota"))
+    soma =+ notas[i]
+    i+=1
+   
+media= soma/4
+
+notas.sort
+print(f"a Maior nota é {notas[-1]}")
+print(f"a Menor nota é {notas[1]}")
+print("A Media é: ",media)
+
+print(notas)
+
+
+
+
+# %%
+
+
